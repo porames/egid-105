4b005b68ee1838cf6f607d0d3562ddbe07d8b3a8
unknown <porames.vat@student.mahidol.edu>
2021-05-23 23:16:59 +0700

    calculator app

---
+++ calculator app/calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.answer = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.n7);
+            this.flowLayoutPanel1.Controls.Add(this.n8);
+            this.flowLayoutPanel1.Controls.Add(this.n9);
+            this.flowLayoutPanel1.Controls.Add(this.n4);
+            this.flowLayoutPanel1.Controls.Add(this.n5);
+            this.flowLayoutPanel1.Controls.Add(this.n6);
+            this.flowLayoutPanel1.Controls.Add(this.n1);
+            this.flowLayoutPanel1.Controls.Add(this.n2);
+            this.flowLayoutPanel1.Controls.Add(this.n3);
+            this.flowLayoutPanel1.Controls.Add(this.n0);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(70, 125);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(360, 419);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // n7
+            // 
+            this.n7.Location = new System.Drawing.Point(3, 3);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(112, 97);
+            this.n7.TabIndex = 0;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = true;
+            this.n7.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n8
+            // 
+            this.n8.Location = new System.Drawing.Point(121, 3);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(112, 97);
+            this.n8.TabIndex = 1;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = true;
+            this.n8.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n9
+            // 
+            this.n9.Location = new System.Drawing.Point(239, 3);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(112, 97);
+            this.n9.TabIndex = 2;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = true;
+            this.n9.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n4
+            // 
+            this.n4.Location = new System.Drawing.Point(3, 106);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(112, 97);
+            this.n4.TabIndex = 3;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = true;
+            this.n4.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n5
+            // 
+            this.n5.Location = new System.Drawing.Point(121, 106);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(112, 97);
+            this.n5.TabIndex = 4;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = true;
+            this.n5.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n6
+            // 
+            this.n6.Location = new System.Drawing.Point(239, 106);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(112, 97);
+            this.n6.TabIndex = 5;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = true;
+            this.n6.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n1
+            // 
+            this.n1.Location = new System.Drawing.Point(3, 209);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(112, 97);
+            this.n1.TabIndex = 8;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = true;
+            this.n1.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n2
+            // 
+            this.n2.Location = new System.Drawing.Point(121, 209);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(112, 97);
+            this.n2.TabIndex = 6;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = true;
+            this.n2.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n3
+            // 
+            this.n3.Location = new System.Drawing.Point(239, 209);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(112, 97);
+            this.n3.TabIndex = 9;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = true;
+            this.n3.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // n0
+            // 
+            this.n0.Location = new System.Drawing.Point(3, 312);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(112, 97);
+            this.n0.TabIndex = 10;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = true;
+            this.n0.Click += new System.EventHandler(this.numpad_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.plus);
+            this.flowLayoutPanel2.Controls.Add(this.minus);
+            this.flowLayoutPanel2.Controls.Add(this.multiply);
+            this.flowLayoutPanel2.Controls.Add(this.equal);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(427, 123);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(142, 421);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(3, 3);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(112, 97);
+            this.plus.TabIndex = 3;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.operation_click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(3, 106);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(112, 97);
+            this.minus.TabIndex = 4;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.operation_click);
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(3, 209);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(112, 97);
+            this.multiply.TabIndex = 5;
+            this.multiply.Text = "x";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.operation_click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(3, 312);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(112, 97);
+            this.equal.TabIndex = 6;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.return_click);
+            // 
+            // answer
+            // 
+            this.answer.AutoSize = true;
+            this.answer.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.answer.Location = new System.Drawing.Point(70, 37);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(0, 74);
+            this.answer.TabIndex = 2;
+            this.answer.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 574);
+            this.Controls.Add(this.answer);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Label answer;
+        private System.Windows.Forms.Button equal;
+    }
+}
+
---
+++ calculator app/calculator/calculator/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        int calculation;
+        string operation = null;
+        string saved_input = null;
+        string new_input = null;
+        private void operation_click(object sender, EventArgs e)
+        {
+            if (saved_input != null)
+            {
+                string s = (sender as Button).Text;
+                operation = s;
+                answer.Text = answer.Text + s;
+            }
+            
+        }
+        private void return_click(object sender, EventArgs e)
+        {
+            if (saved_input != null)
+            {
+                answer.Text = Convert.ToString(Convert.ToInt32(saved_input) + Convert.ToInt32(new_input));
+            }
+        }
+        private void numpad_Click(object sender, EventArgs e)
+        {
+            string s = (sender as Button).Text;
+            if (operation == null)
+            {
+
+                saved_input = saved_input + s;
+            }
+            else
+            {
+                new_input = new_input+s;
+            }
+            answer.Text = answer.Text + s;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("button1 was clicked");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
