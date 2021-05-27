b28d4fcc5663f71b159fcfe4b28827551619119b
unknown <porames.vat@student.mahidol.edu>
2021-05-27 16:24:39 +0700

    update arduino code

--- arduino/joystick/joystick.ino
+++ arduino/joystick/joystick.ino
@@ -1,25 +1,41 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(2, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 unsigned long previousMillis = 0;
 int ledState = LOW;
+int countPressed = 0;
+boolean lastSw;
+int state = 0;
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned long Vx = analogRead(0);
   unsigned long Vy = analogRead(1);
   int sw = digitalRead(2);
-  Serial.print("Vx: ");
-  Serial.print(Vx);
-  Serial.print(" Vy: ");
-  Serial.print(Vy);
-  Serial.print(" sw: ");
-  Serial.println(sw);
-  if (sw == 1) {
+  int freq;
+  int xDeg=map(Vx,0,1023,0,180);
+  int yDeg=map(Vy,0,1023,0,180);
+  Serial.print(xDeg);  
+  Serial.print(", ");
+  Serial.println(yDeg); 
+
+  if (sw && !lastSw) {
+    countPressed++;
+    state = countPressed % 3;
+    Serial.println(state);
+  }
+
+  if (state != 2) {
+    if (state == 0) {
+      freq = 250;
+    }
+    else if (state == 1) {
+      freq = 1000;
+    }
     unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= 1000) {
+    if (currentMillis - previousMillis >= freq) {
       previousMillis = currentMillis;
       if (ledState == LOW) {
         ledState = HIGH;
@@ -31,8 +47,9 @@ void loop() {
     }
   }
   else{
-      ledState = LOW;
-      digitalWrite(LED_BUILTIN, ledState);
+    ledState = LOW;
+    digitalWrite(LED_BUILTIN, ledState);
   }
+  lastSw = sw;
 
 }
